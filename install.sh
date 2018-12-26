9ac69e9df2117ff8df7a0eb76938f5f583dfc7f0
Garcez <allan.f.garcez@gmail.com>
2018-12-26 01:29:49 -0200

    init install script

---
+++ install.sh
@@ -0,0 +1,43 @@
+#
+# ██████╗  ██████╗ ████████╗███████╗██╗██╗     ███████╗███████╗
+# ██╔══██╗██╔═══██╗╚══██╔══╝██╔════╝██║██║     ██╔════╝██╔════╝
+# ██║  ██║██║   ██║   ██║   █████╗  ██║██║     █████╗  ███████╗
+# ██║  ██║██║   ██║   ██║   ██╔══╝  ██║██║     ██╔══╝  ╚════██║
+# ██████╔╝╚██████╔╝   ██║   ██║     ██║███████╗███████╗███████║
+# ╚═════╝  ╚═════╝    ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝
+# Dotfiles by: @vandalvnl
+# ToDo:
+## install rofi
+## install polybar
+## configure KDE shortcuts
+## install vscode theme and plugins|shortcuts
+
+git clone https://github.com/vandalvnl/dotfiles $HOME/dotfiles
+cd $HOME/dotfiles
+
+# ZSH Config
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cat zsh/zshrc > ~/.zshrc
+cp ~/.oh-my-zsh/themes/ zsh/vandalvnl.zsh-theme
+cd $ZSH_CUSTOM/plugins && git clone https://github.com/chrissicool/zsh-256color
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+echo 'export NVM_DIR="${XDG_CONFIG_HOME/:-$HOME/.}nvm"' >> ~/.zshrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.zshrc
+nvm install node
+npm install -g npm create-react-app typescript expo-cli wifi spoof serve http-server
+
+# SDKMAN
+curl -s "https://get.sdkman.io" | zsh
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java
+sdk install maven
+sdk install gradle
+sdk install sbt
+sdk install scala
+
+# Misc Stuff
+cp curlrc ~/.curlrc
