4083e1c7acb27fbeda2462e964e5ca7120479b87
g4rcez <allan.f.garcez@gmail.com>
2020-05-05 03:51:45 -0300

    migrate to xfce4

--- polybar/scripts/shutdown-menu.sh
+++ polybar/scripts/shutdown-menu.sh
@@ -1,7 +1,7 @@
-MENU="$(rofi -sep '|' -dmenu -i -p 'System' -location 0 -yoffset 20 -xoffset -0 -width 20 -hide-scrollbar -line-padding 4 -padding 20 -lines 4 <<< ' Lock| Logout| Reboot| Shutdown' -theme /home/garcez/dotfiles/rofi/rofi-menus/themes/appsmenu.rasi)"
+MENU="$(rofi -sep '|' -dmenu -i -p 'System' -location 0 -yoffset 20 -xoffset -0 -width 20 -hide-scrollbar -line-padding 4 -padding 20 -lines 4 <<< 'Lock|Logout|Reboot|Shutdown' -theme /home/g4rcez/dotfiles/rofi/rofi-menus/themes/appsmenu.rasi)"
 case "$MENU" in
-    *Lock) betterlockscreen -l dimblur -t Dica: "Não use" ;;
-    *Logout) i3exit logout, mode "default" ;;
+    *Lock) i3lock -c 000000 ;;
+    *Logout) i3exit logout ;;
     *Reboot) reboot ;;
     *Shutdown) systemctl -i poweroff
 esac
