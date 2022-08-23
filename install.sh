ee092deed100566ec46aaf443018c914bda03c9b
g4rcez <allan.f.garcez@gmail.com>
2022-08-23 03:02:26 -0300

    upgrade

--- install.sh
+++ install.sh
@@ -7,20 +7,17 @@
 # ╚═════╝  ╚═════╝    ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝
 # Dotfiles by: @vandalvnl
 # ToDo:
-## install rofi
-## install polybar
-## configure KDE shortcuts
-## install vscode theme and plugins|shortcuts
-
-git clone https://github.com/g4rcez/dotfiles $HOME/dotfiles
-cd $HOME/dotfiles
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#git clone https://github.com/g4rcez/dotfiles $HOME/dotfiles
+#cd $HOME/dotfiles
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 git clone https://github.com/chrissicool/zsh-256color $ZSH_CUSTOM/plugins/zsh-256color
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 
 # Misc Stuff
 cp curlrc ~/.curlrc
