bcc9876242695392b6c2244cf347387682e6c259
g4rcez <allan.f.garcez@gmail.com>
2022-10-26 22:20:10 -0300

    feat

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ git clone https://github.com/chrissicool/zsh-256color $ZSH_CUSTOM/plugins/zsh-25
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone git@github.com:grigorii-zander/zsh-npm-scripts-autocomplete.git ~/.oh-my-zsh/custom/plugins/zsh-npm-scripts-autocomplete
+git clone https://github.com/popstas/zsh-command-time.git ~/.oh-my-zsh/custom/plugins/command-time
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
@@ -32,7 +33,15 @@ ln -sf $HOME/dotfiles/idea/.ideavimrc $HOME/.ideavimrc
 ln -sf $HOME/dotfiles/global_gitignore $HOME/.gitignore
 ln -sf $HOME/dotfiles/vim/vimrc $HOME/.vimrc
 
-
 # Git
 git config --global core.excludesFile '~/.gitignore'
 
+# Manjaro install
+sudo pacman -S base-devel flameshot vim neovim
+
+# Node installs
+volta install node
+volta pin node
+volta install yarn
+volta pin yarn
+npm i -g pnpm add-gitignore
