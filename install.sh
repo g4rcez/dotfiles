98974013798f4f4a46ce239075a94fe41fc6364f
g4rcez <allan.f.garcez@gmail.com>
2022-12-28 00:51:10 -0300

    wip

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@
 # ╚═════╝  ╚═════╝    ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝
 # Dotfiles by: @vandalvnl
 # ToDo:
-git clone https://github.com/g4rcez/dotfiles $HOME/dotfiles
-cd $HOME/dotfiles
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# git clone https://github.com/g4rcez/dotfiles $HOME/dotfiles
+# cd $HOME/dotfiles
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 git clone https://github.com/chrissicool/zsh-256color $ZSH_CUSTOM/plugins/zsh-256color
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
@@ -38,8 +38,8 @@ git config --global init.defaultBranch main
 
 
 # Manjaro install
-sudo pacman -S base-devel bat flameshot vim neovim ocs-url docker python-pip jq docker-compose screenkey python-pip yay rofi rofi-emoji rofi-calc
-yay -S insomnia lastpass-cli albert k6 visual-studio-code-bin
+sudo pacman -S base-devel bat flameshot vim neovim ocs-url docker python-pip jq docker-compose screenkey python-pip yay
+yay -S insomnia albert k6 visual-studio-code-bin
 
 # Node installs
 volta install node
@@ -51,20 +51,8 @@ npm i -g pnpm add-gitignore neovim
 ## Python pip modules
 pip install --user colour docker numpy pint inflect matplotlib fuzzywuzzy wheel meme
 
-# Theme
-cd /tmp
-clone vinceliuice/Colloid-gtk-theme
-cd Colloid-gtk-theme
-./install.sh -c dark -s compact -l --tweaks black --tweaks rimless
-
 # Lunarvim
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 ln -sf $HOME/dotfiles/lvim/config.lua $HOME/.config/lvim/config.lua
 
-# Go
-go install github.com/codesenberg/bombardier@latest
 ln -sf $HOME/dotfiles/dygma $HOME/Raise
-
-# rofi
-git clone --depth=1 https://github.com/adi1090x/rofi.git
-echo "PATH=$PATH:~/.config/rofi/scripts" >> ~/.profile
