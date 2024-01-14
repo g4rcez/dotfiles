619a3450318042997c58c6f0112487bc466a1a52
g4rcez <allan.f.garcez@gmail.com>
2024-01-14 04:32:04 -0300

    wip: work in progress

--- install.sh
+++ install.sh
@@ -7,49 +7,51 @@
 # ██████╔╝╚██████╔╝   ██║   ██║     ██║███████╗███████╗███████║
 # ╚═════╝  ╚═════╝    ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝
 ########################################################################################################################
-
 set -xe
-mkdir -p $HOME/tools $HOME/.tmp
+mkdir -p $HOME/tools $HOME/.tmp;
+
+function checkcommand () {
+  if [[ -f "$(command -v $1)" ]]; then
+    echo "fzf installed"
+  else
+    $2
+  fi
+}
 ########################################################################################################################
 ##### fzf
-if [[ -f "$(command -v fzf)" ]]; then
-  echo "fzf installed"
-else
+function installfzf() {
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install
-fi
-
+}
+checkcommand "fzf" installfzf
 ########################################################################################################################
 ##### brew
-if [[ -f "$(command -v brew)" ]]; then
-  echo "brew installed"
-else
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  /bin/bash $HOME/dotfiles/setup/brew.sh
-fi
-
+function installbrew() {
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  bash $HOME/dotfiles/setup/brew.sh
+}
+checkcommand "brew" installbrew
 ########################################################################################################################
 ##### node config
-curl https://get.volta.sh | bash
-volta install node@latest
-volta install yarn@1.22.1
-npm i -g pnpm ts-node yarn neovim
-
+function installvolta() {
+  curl https://get.volta.sh | bash
+  volta install node@latest
+  volta install yarn@1.22.1
+  npm i -g pnpm ts-node yarn neovim
+}
+checkcommand "volta" installvolta
 ########################################################################################################################
 ##### python modules
 pip3 install --user colour docker numpy pint inflect matplotlib fuzzywuzzy wheel meme
 
 ########################################################################################################################
 ##### lazyvim
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
 ln -sf $HOME/dotfiles/nvim/ $HOME/.config/nvim/
-ln -sf $HOME/dotfiles/vim/vimrc $HOME/.vimrc
 
 ########################################################################################################################
 ##### symlinks
 # ln -sf $HOME/dotfiles/config/kitty $HOME/.config/kitty
-# ln -sf $HOME/dotfiles/config/wezterm/main.lua $HOME/.wezterm.lua
+ln -sf $HOME/dotfiles/config/wezterm/wezterm.lua $HOME/.wezterm.lua
 ln -sf $HOME/dotfiles/config/fd/.fdignore $HOME/.fdignore
 ln -sf $HOME/dotfiles/config/starship.toml $HOME/.config/starship.toml
 ln -sf $HOME/dotfiles/idea/.ideavimrc $HOME/.ideavimrc
@@ -67,4 +69,8 @@ git config --global init.defaultBranch main
 ###### misc
 
 ## atuin history
-bash <(curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh)
+function installatuin() {
+  bash <(curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh)
+}
+checkcommand "atuin" installatuin
+
