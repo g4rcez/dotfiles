587ca00af21efba456b9158c726ea56e85c8ba0a
g4rcez <allan.f.garcez@gmail.com>
2024-05-07 02:55:03 -0300

    wip: checkpoint at 2024-05-07T02:55:03 TZ-03(Tue, 128)

--- setup/osx.sh
+++ setup/osx.sh
@@ -1,4 +1,3 @@
-#ln -sf $HOME/dotfiles/espanso "$HOME/Library/Application\ Support/espanso/match/base.yml"
 # "Disable press-and-hold for keys in favor of key repeat"
 defaults write -g ApplePressAndHoldEnabled -bool false
 
