a0ebfd2fcc424de7d1aa74648dae72b2e86c9f5b
g4rcez <allan.f.garcez@gmail.com>
2024-05-07 02:44:45 -0300

    wip: checkpoint at 2024-05-07T02:44:45 TZ-03(Tue, 128)

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -31,22 +31,22 @@ alias path='echo -e ${PATH//:/\\n}'
 ############################################################################
 ## vim
 if [ -x "$(command -v nvim)" ]; then
-  alias vim="nvim"
+    alias vim="nvim"
 fi
 
 function zshrc() {
-  nvim "$HOME/dotfiles/zsh/zshrc"
+    nvim "$HOME/dotfiles/zsh/zshrc"
 }
 
 function fvim() {
-  nvim "$(fzf)"
+    nvim "$(fzf)"
 }
 
 function codi() {
-  local syntax="${1:-python}"
-  shift
-  nvim -c \
-    "let g:startify_disable_at_vimenter = 1 |\
+    local syntax="${1:-python}"
+    shift
+    nvim -c \
+        "let g:startify_disable_at_vimenter = 1 |\
     set bt=nofile ls=0 noru nonu nornu |\
     hi ColorColumn ctermbg=NONE |\
     hi VertSplit ctermbg=NONE |\
@@ -57,8 +57,8 @@ function codi() {
 ############################################################################
 ## linux
 if [[ "$(uname)" != "Darwin" ]]; then
-  alias pbcopy='xclip -sel clip'
-  alias pbpaste='xclip -selection clipboard -o'
+    alias pbcopy='xclip -sel clip'
+    alias pbpaste='xclip -selection clipboard -o'
 fi
 ############################################################################
 ## network
@@ -70,112 +70,118 @@ alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[
 ## docker
 alias docker-compose="docker compose"
 function docker-prune-volumes() {
-  docker volume rm "$(docker volume ls -q --filter dangling=true)"
+    docker volume rm "$(docker volume ls -q --filter dangling=true)"
 }
 
 function dockerkill() {
-  docker kill "$(docker ps -q)"
+    docker kill "$(docker ps -q)"
 }
 
 function fdocker() {
-  docker ps -a | sed 1d | fzf -q "$1" --no-sort -m --tac | awk '{ print $1 }' | xargs -r docker rm
+    docker ps -a | sed 1d | fzf -q "$1" --no-sort -m --tac | awk '{ print $1 }' | xargs -r docker rm
 }
 
 ############################################################################
 ## osx
 if [[ "$(uname)" == "Darwin" ]]; then
-  # macOS has no `md5sum`, so use `md5` as a fallback
-  command -v md5sum >/dev/null || alias md5sum="md5"
-  # macOS has no `sha1sum`, so use `shasum` as a fallback
-  command -v sha1sum >/dev/null || alias sha1sum="shasum"
-  command -v hd >/dev/null || alias hd="hexdump -C"
-  alias dsclean="find . -type f -name '*.DS_Store' -ls -delete"
+    # macOS has no `md5sum`, so use `md5` as a fallback
+    command -v md5sum >/dev/null || alias md5sum="md5"
+    # macOS has no `sha1sum`, so use `shasum` as a fallback
+    command -v sha1sum >/dev/null || alias sha1sum="shasum"
+    command -v hd >/dev/null || alias hd="hexdump -C"
+    alias dsclean="find . -type f -name '*.DS_Store' -ls -delete"
 
-  function flush() {
-    sudo dscacheutil -flushcache
-    sudo killall -HUP mDNSResponder
-  }
+    function flush() {
+        sudo dscacheutil -flushcache
+        sudo killall -HUP mDNSResponder
+    }
 fi
 
 ############################################################################
 ## functions
 function extract() {
-  FILE="$1"
-  if [ -f "$FILE" ]; then
-    case $FILE in
-    *.tar.bz2) tar xjf "$FILE" ;;
-    *.tar.gz) tar xzf "$FILE" ;;
-    *.bz2) bunzip2 "$FILE" ;;
-    *.rar) unrar x "$FILE" ;;
-    *.gz) gunzip "$FILE" ;;
-    *.tar) tar xf "$FILE" ;;
-    *.tbz2) tar xjf "$FILE" ;;
-    *.tgz) tar xzf "$FILE" ;;
-    *.zip) unzip "$FILE" ;;
-    *.Z) uncompress "$FILE" ;;
-    *.7z) 7z x "$FILE" ;;
-    *) echo "'$FILE' cannot be extracted via ex()" ;;
-    esac
-  else
-    echo "'$FILE' is not a valid file"
-  fi
+    FILE="$1"
+    if [ -f "$FILE" ]; then
+        case $FILE in
+        *.tar.bz2) tar xjf "$FILE" ;;
+        *.tar.gz) tar xzf "$FILE" ;;
+        *.bz2) bunzip2 "$FILE" ;;
+        *.rar) unrar x "$FILE" ;;
+        *.gz) gunzip "$FILE" ;;
+        *.tar) tar xf "$FILE" ;;
+        *.tbz2) tar xjf "$FILE" ;;
+        *.tgz) tar xzf "$FILE" ;;
+        *.zip) unzip "$FILE" ;;
+        *.Z) uncompress "$FILE" ;;
+        *.7z) 7z x "$FILE" ;;
+        *) echo "'$FILE' cannot be extracted via ex()" ;;
+        esac
+    else
+        echo "'$FILE' is not a valid file"
+    fi
 }
 
 function listening() {
-  if [ $# -eq 0 ]; then
-    sudo lsof -iTCP -sTCP:LISTEN -n -P
-  elif [ $# -eq 1 ]; then
-    sudo lsof -iTCP -sTCP:LISTEN -n -P | grep -i --color "$1"
-  else
-    echo "Usage: listening [pattern]"
-  fi
+    if [ $# -eq 0 ]; then
+        sudo lsof -iTCP -sTCP:LISTEN -n -P
+    elif [ $# -eq 1 ]; then
+        sudo lsof -iTCP -sTCP:LISTEN -n -P | grep -i --color "$1"
+    else
+        echo "Usage: listening [pattern]"
+    fi
 }
 
 function updateAll() {
-  znap pull
-  brew upgrade
-  fzf-update
+    znap pull
+    brew upgrade
+    fzf-update
 }
 
 function secretuuid() {
-  echo -n "$1" | openssl enc -e -aes-256-cbc -a -salt | base64
+    echo -n "$1" | openssl enc -e -aes-256-cbc -a -salt | base64
 }
 
 # Determine size of a file or total size of a directory
 function fs() {
-  if du -b /dev/null >/dev/null 2>&1; then
-    local arg=-sbh
-  else
-    local arg=-sh
-  fi
-  if [[ -n "$@" ]]; then
-    du $arg -- "$@"
-  else
-    du $arg .[^.]* ./*
-  fi
+    if du -b /dev/null >/dev/null 2>&1; then
+        local arg=-sbh
+    else
+        local arg=-sh
+    fi
+    if [[ -n "$@" ]]; then
+        du $arg -- "$@"
+    else
+        du $arg .[^.]* ./*
+    fi
 }
 
 function dotenv() {
-  if [[ -f "$1" ]]; then
-    set -o allexport
-    source "$1"
-    set +o allexport
-  fi
+    if [[ -f "$1" ]]; then
+        set -o allexport
+        source "$1"
+        set +o allexport
+    fi
 }
 
 function memory() {
-  ps -eo size,pid,user,command --sort -size | awk '{ hr=$1/1024 ; printf("%13.2f MB ",hr) } { for ( x=4 ; x<=NF ; x++ ) { printf("%s ",$x) } print "" }'
+    ps -eo size,pid,user,command --sort -size | awk '{ hr=$1/1024 ; printf("%13.2f MB ",hr) } { for ( x=4 ; x<=NF ; x++ ) { printf("%s ",$x) } print "" }'
 }
 
 function cdm() {
-  mkdir -p "$1"
-  cd "$1" || return
+    mkdir -p "$1"
+    cd "$1" || return
 }
 
 function cpv() {
-  rsync -pogbr -hhh --backup-dir="$HOME/.tmp" -e /dev/null --progress "$@"
+    rsync -pogbr -hhh --backup-dir="$HOME/.tmp" -e /dev/null --progress "$@"
 }
 
 function anon() {
-  ZSH_AUTOSUGGEST_STRATEGY=()
+    ZSH_AUTOSUGGEST_STRATEGY=()
+}
+
+function karabiner-reset() {
+    launchctl stop org.pqrs.karabiner.karabiner_console_user_server
+    sleep 0.5
+    launchctl start org.pqrs.karabiner.karabiner_console_user_server
 }
--- zsh/zellij.sh
+++ zsh/zellij.sh
@@ -55,3 +55,17 @@ function _zellij_tab_name_update() {
         command nohup zellij action rename-tab "$tab_name" >/dev/null 2>&1
     fi
 }
+
+function zr() { zellij run --name "$*" -- zsh -ic "$*"; }
+function zrf() { zellij run --name "$*" --floating -- zsh -ic "$*"; }
+function zri() { zellij run --name "$*" --in-place -- zsh -ic "$*"; }
+function ze() { zellij edit "$*"; }
+function zef() { zellij edit --floating "$*"; }
+function zei() { zellij edit --in-place "$*"; }
+function zpipe() {
+    if [ -z "$1" ]; then
+        zellij pipe
+    else
+        zellij pipe -p $1
+    fi
+}
