588c6a2820583cb81a6d56b5239e0cbf24d07d6c
g4rcez <allan.f.garcez@gmail.com>
2024-12-09 18:31:52 -0300

    chore: commit before nix install

---
+++ nix/flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Setup osx";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ 
+            pkgs.vim
+        ];
+
+      # Necessary for using flakes on this system.
+      services.nix-daemon.enable = true
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      programs.zsh.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#garcez
+    darwinConfigurations."garcez" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+    darwinPackages = self.darwinConfigurations."garcez".pkgs;
+  };
+}
