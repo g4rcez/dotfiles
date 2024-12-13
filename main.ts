7b96fbf9c24dca70b050484105ded79d2a822fef
g4rcez <allan.f.garcez@gmail.com>
2024-12-13 00:40:00 -0300

    feat: move nvim to config/nvim

--- main.ts
+++
@@ -1,9 +0,0 @@
-import { karabiner } from "./src/karabiner/karabiner.ts";
-import { espanso } from "./src/espanso/espanso.ts";
-
-async function main() {
-    await espanso();
-    karabiner();
-}
-
-main().then(() => console.log("✅ Config files generated"));
