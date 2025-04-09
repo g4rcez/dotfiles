39a190b81e2d45d8bed8b0cb2760ba20c39beb40
g4rcez <allan.f.garcez@gmail.com>
2025-04-09 10:53:17 -0300

    feat: 09/04/2025 updates

--- config/nvim/lua/custom/plugins/lsp.lua
+++ config/nvim/lua/custom/plugins/lsp.lua
@@ -10,6 +10,11 @@ return {
         },
     },
     {
+        opts = {},
+        "pmizio/typescript-tools.nvim",
+        dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    },
+    {
         "williamboman/mason.nvim",
         opts = {
             ensure_installed = {
@@ -206,27 +211,27 @@ return {
                     end,
                 },
             }
-            -- require("typescript-tools").setup {
-            --     settings = {
-            --         separate_diagnostic_server = true,
-            --         publish_diagnostic_on = "insert_leave",
-            --         expose_as_code_action = {},
-            --         tsserver_path = nil,
-            --         tsserver_plugins = {},
-            --         tsserver_max_memory = "auto",
-            --         tsserver_format_options = {},
-            --         tsserver_file_preferences = {},
-            --         tsserver_locale = "en",
-            --         complete_function_calls = false,
-            --         include_completions_with_insert_text = true,
-            --         code_lens = "on",
-            --         disable_member_code_lens = true,
-            --         jsx_close_tag = {
-            --             enable = false,
-            --             filetypes = { "javascriptreact", "typescriptreact" },
-            --         },
-            --     },
-            -- }
+            require("typescript-tools").setup {
+                settings = {
+                    separate_diagnostic_server = true,
+                    publish_diagnostic_on = "insert_leave",
+                    expose_as_code_action = {},
+                    tsserver_path = nil,
+                    tsserver_plugins = {},
+                    tsserver_max_memory = "auto",
+                    tsserver_format_options = {},
+                    tsserver_file_preferences = {},
+                    tsserver_locale = "en",
+                    complete_function_calls = false,
+                    include_completions_with_insert_text = true,
+                    code_lens = "on",
+                    disable_member_code_lens = true,
+                    jsx_close_tag = {
+                        enable = false,
+                        filetypes = { "javascriptreact", "typescriptreact" },
+                    },
+                },
+            }
             local schemas = require "schemastore"
             require("lspconfig").jsonls.setup {
                 settings = {
