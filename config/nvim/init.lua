2e18eff1fb53c407cc30a8da954e431c36581990
g4rcez <allan.f.garcez@gmail.com>
2025-05-30 01:45:57 -0300

    feat: nvim + ghostty updates

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -20,29 +20,38 @@
 =====================================================================
 --]]
 
-if vim.env.VSCODE then
-    vim.g.vscode = true
+local function vscode()
+    require "config.options"
+    require "config.autocmd"
+    require "config.vscode"
 end
 
-require "config.options"
-require "config.autocmd"
+local function nvim()
+    require "config.options"
+    require "config.autocmd"
+    local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+    if not (vim.uv or vim.loop).fs_stat(lazypath) then
+        local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+        local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
+        if vim.v.shell_error ~= 0 then
+            error("Error cloning lazy.nvim:\n" .. out)
+        end
+    end ---@diagnostic disable-next-line: undefined-field
+    vim.opt.rtp:prepend(lazypath)
 
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
-    if vim.v.shell_error ~= 0 then
-        error("Error cloning lazy.nvim:\n" .. out)
-    end
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
+    require("lazy").setup {
+        checker = { enabled = true },
+        spec = {
+            { import = "custom.plugins" },
+        },
+        defaults = {
+            version = false,
+        },
+    }
+end
 
-require("lazy").setup {
-    checker = { enabled = true },
-    spec = {
-        { import = "custom.plugins" },
-    },
-    defaults = {
-        version = false,
-    },
-}
+if vim.g.vscode then
+    vscode()
+else
+    nvim()
+end
--- config/nvim/lua/custom/plugins/whichkey.lua
+++ config/nvim/lua/custom/plugins/whichkey.lua
@@ -6,22 +6,9 @@ return {
         opts = function(_, defaultOpts)
             defaultOpts.preset = "helix"
             local wk = require "which-key"
-            local DEFAULT_OPTS = { noremap = true, silent = true }
-            local function keymap(mode, from, to, opts)
-                opts = opts or {}
-                opts.silent = opts.silent ~= true
-                opts.noremap = opts.noremap ~= true
-                vim.keymap.set(mode, from, to, opts)
-            end
-            vim.api.nvim_create_autocmd("User", {
-                pattern = "OilActionsPost",
-                callback = function(event)
-                    if event.data.actions.type == "move" then
-                        local snacks = require "snacks"
-                        snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
-                    end
-                end,
-            })
+            local createKeyMap = require "config.keymap"
+            local keymap = createKeyMap(wk)
+            local bind = keymap.bind
 
             local function groups()
                 wk.add { "<leader>b", group = "[b]uffer", icon = "󱦞" }
@@ -35,94 +22,6 @@ return {
                 wk.add { "<leader>r", group = "[r]eplace", mode = { "n" }, icon = "" }
                 wk.add { "<leader>x", group = "[x]errors", mode = { "n" }, icon = "" }
                 wk.add { "<leader>n", group = "[n]ew cursor", mode = { "n" }, icon = "" }
-            end
-
-            local bind = {
-                normal = function(from, action, opts)
-                    local o = opts or {}
-                    local desc = o.desc or ""
-                    keymap("n", from, action, { desc = desc })
-                    wk.add { from, action, desc = desc or "", mode = { "n" }, icon = o.icon }
-                end,
-                x = function(from, to, opts)
-                    keymap("x", from, to, opts)
-                    wk.add { from, to, desc = opts.desc or "", mode = { "x" }, icon = opts.icon }
-                end,
-                cmd = function(from, to, opts)
-                    keymap("c", from, to, opts)
-                    wk.add { from, to, desc = opts.desc or "", mode = { "c" }, icon = opts.icon }
-                end,
-                insert = function(from, to, opts)
-                    keymap("i", from, to, opts)
-                    wk.add { from, to, desc = opts.desc or "", mode = { "i" }, icon = opts.icon }
-                end,
-                visual = function(from, to, opts)
-                    keymap("v", from, to, opts)
-                    wk.add { from, to, desc = opts.desc or "", mode = { "v" }, icon = opts.icon }
-                end,
-            }
-
-            local function defaults()
-                bind.normal("J", "mzJ`z", { desc = "Primeagen join lines" })
-                bind.cmd("<C-A>", "<HOME>", { desc = "Go to HOME in command" })
-                bind.insert("<C-A>", "<HOME>", { desc = "Go to home in insert" })
-                bind.insert("<C-E>", "<END>", { desc = "Go to end in insert" })
-                bind.normal("<C-s>", "<cmd>:w<CR>", { desc = "Save" })
-                bind.insert("<C-s>", "<Esc>:w<CR>a", { desc = "Save" })
-                bind.insert("<C-z>", "<Esc>ua", { desc = "Go to end in insert" })
-                bind.normal("#", "#zz", { desc = "Center previous pattern" })
-                bind.normal("*", "*zz", { desc = "Center next pattern" })
-                bind.normal("+", "<C-a>", { desc = "Increment" })
-                bind.normal("-", "<C-x>", { desc = "Decrement" })
-                bind.normal("0", "^", { desc = "Goto first non-whitespace" })
-                bind.visual("0", "^", { desc = "Goto first non-whitespace" })
-                bind.normal("<BS>", '"_', { desc = "BlackHole register" })
-                bind.visual("<BS>", '"_', { desc = "BlackHole register" })
-                bind.normal(">", ">>", { desc = "Indent" })
-                bind.normal("<", "<<", { desc = "Deindent" })
-                bind.normal("vv", "V", { desc = "Select line" })
-                bind.normal("j", "gj", DEFAULT_OPTS)
-                bind.normal("k", "gk", DEFAULT_OPTS)
-                bind.visual("<", "<gv", DEFAULT_OPTS)
-                bind.visual("<leader>sr", "<cmd>!tail -r<CR>", { desc = "Reverse sort lines" })
-                bind.visual("<leader>ss", "<cmd>sort<CR>", { desc = "Sort lines" })
-                bind.visual(">", ">gv", DEFAULT_OPTS)
-                bind.x("p", [["_dP]], DEFAULT_OPTS)
-                bind.normal("<Esc>", "<cmd>nohlsearch<CR>", { desc = "No hlsearch" })
-                bind.insert("<Esc>", "<C-c>", { desc = "normal mode", noremap = true, silent = true })
-                bind.normal("<leader>cq", vim.diagnostic.setloclist, { desc = "Open diagnostic [c]ode [q]uickfix list" })
-            end
-
-            local function navigate(n)
-                local current = vim.api.nvim_get_current_buf()
-                for i, v in ipairs(vim.t.bufs) do
-                    if current == v then
-                        local new_buf = vim.t.bufs[(i + n - 1) % #vim.t.bufs + 1]
-                        if new_buf ~= current then
-                            vim.api.nvim_set_current_buf(new_buf)
-                        end
-                        return
-                    end
-                end
-            end
-
-            local function buffers()
-                bind.normal("<leader>qq", "<cmd>bdelete<CR>", { desc = "[q]uit tab", icon = "󰅛" })
-                bind.normal("<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete current buffer", icon = "󰅛" })
-                bind.normal("<C-h>", function()
-                    navigate(-1)
-                end, DEFAULT_OPTS)
-                bind.normal("<C-l>", function()
-                    navigate(1)
-                end, DEFAULT_OPTS)
-                bind.normal(
-                    "<leader>bo",
-                    require("snacks.bufdelete").other,
-                    { desc = "Close all except current", icon = "" }
-                )
-                bind.normal("<leader>bh", function()
-                    require("treesitter-context").go_to_context(vim.v.count1)
-                end, { silent = true, desc = "[h]eader of context" })
             end
 
             local function harpoonConfig()
@@ -175,10 +74,10 @@ return {
             end
 
             local function rename_once()
-                local clients = vim.lsp.get_clients({ bufnr = 0 })
+                local clients = vim.lsp.get_clients { bufnr = 0 }
                 local client_id_to_use = nil
                 for _, client in ipairs(clients) do
-                    if client.supports_method("textDocument/rename") then
+                    if client.supports_method "textDocument/rename" then
                         client_id_to_use = client.id
                         break
                     end
@@ -186,13 +85,19 @@ return {
 
                 if client_id_to_use then
                     local params = vim.lsp.util.make_position_params()
-                    vim.ui.input({ prompt = 'New Name: ', default = vim.fn.expand('<cword>') }, function(new_name)
-                        if not new_name or new_name == "" then return end
+                    vim.ui.input({ prompt = "New Name: ", default = vim.fn.expand "<cword>" }, function(new_name)
+                        if not new_name or new_name == "" then
+                            return
+                        end
                         params.newName = new_name
 
                         vim.lsp.buf_request(0, "textDocument/rename", params, function(err, result, ctx)
-                            if err then return end
-                            if not result then return end
+                            if err then
+                                return
+                            end
+                            if not result then
+                                return
+                            end
                             if ctx.client_id == client_id_to_use then
                                 vim.lsp.util.apply_workspace_edit(result, "utf-8")
                             end
@@ -201,17 +106,15 @@ return {
                 end
             end
 
-
             local function code()
-                bind.normal("<leader>co", "<cmd>TSToolsOrganizeImports<CR>", { desc = "[c]ode [o]rganize" })
                 bind.normal("<leader>rr", function()
                     require("grug-far").open { engine = "astgrep" }
                 end, { desc = "Replace with grug-far astgrep" })
                 bind.normal("]g", function()
-                    vim.diagnostic.goto_next({})
+                    vim.diagnostic.goto_next {}
                 end, { desc = "Goto next error" })
                 bind.normal("[g", function()
-                    vim.diagnostic.goto_prev({})
+                    vim.diagnostic.goto_prev {}
                 end, { desc = "Goto previous error" })
 
                 bind.normal("<leader>cr", rename_once, { desc = "[c]ode [r]ename" })
@@ -225,7 +128,9 @@ return {
                     }
                 end, { desc = "[c]ode [F]ind aerial" })
             end
-            local keymaps = { groups, defaults, buffers, code, harpoonConfig }
+            local createMotions = require "config.motions"
+            local motions = createMotions(keymap)
+            local keymaps = { groups, defaults = motions.defaults(), buffers = motions.buffers(), code, harpoonConfig }
             for _, func in ipairs(keymaps) do
                 func()
             end
