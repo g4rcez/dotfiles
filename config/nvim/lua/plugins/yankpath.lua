f107cc1fefe789f75f2c0648de5aa3b931381862
g4rcez <allan.f.garcez@gmail.com>
2025-12-15 15:40:14 -0300

    wip: checkpoint at 2025-12-15T15:40:14 TZ-03(Mon, 349)

--- config/nvim/lua/plugins/yankpath.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "ywpkwon/yank-path.nvim",
-    cond = not require("config.vscode").isVscode(),
-    config = function()
-        require("yank_path").setup { default_mapping = false }
-        vim.keymap.set("n", "<leader>yp", "<cmd>YankPath<CR>", { desc = "Yank file path" })
-    end,
-}
