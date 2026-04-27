b0fa93f18fb7dc19d2780e660ff5806fb880bbde
g4rcez <allan.f.garcez@gmail.com>
2026-04-27 19:22:44 +0100

    fix(nvim): add augroup to resession autocmds to prevent duplicates on reload

--- config/nvim/lua/plugins/session.lua
+++ config/nvim/lua/plugins/session.lua
@@ -7,9 +7,12 @@ return {
             local resession = require("resession")
             resession.setup(opts)
 
+            local group = vim.api.nvim_create_augroup("ResessionWorkspace", { clear = true })
+
             -- Tag each buffer with the tabpage(s) it was entered in.
             -- bufferline reads vim.b[buf].workspace_tabs to filter per-tab.
             vim.api.nvim_create_autocmd("BufEnter", {
+                group = group,
                 callback = function(ev)
                     local tab = tostring(vim.api.nvim_get_current_tabpage())
                     local tags = vim.b[ev.buf].workspace_tabs or {}
@@ -20,6 +23,7 @@ return {
 
             -- Auto-save all workspaces on exit
             vim.api.nvim_create_autocmd("VimLeavePre", {
+                group = group,
                 callback = function()
                     resession.save("last", { notify = false })
                 end,
