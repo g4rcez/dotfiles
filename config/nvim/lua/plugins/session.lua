abd40f22e866582aba8291527509d2b2fcd8bae8
g4rcez <allan.f.garcez@gmail.com>
2026-04-27 19:21:26 +0100

    feat(nvim): replace persistence.nvim with resession.nvim for workspace sessions

--- config/nvim/lua/plugins/session.lua
+++ config/nvim/lua/plugins/session.lua
@@ -1,8 +1,29 @@
 return {
     {
-        cond = not require("config.vscode").isVscode(),
-        "folke/persistence.nvim",
-        event = "BufReadPre",
+        "stevearc/resession.nvim",
+        lazy = false,
         opts = {},
+        config = function(_, opts)
+            local resession = require("resession")
+            resession.setup(opts)
+
+            -- Tag each buffer with the tabpage(s) it was entered in.
+            -- bufferline reads vim.b[buf].workspace_tabs to filter per-tab.
+            vim.api.nvim_create_autocmd("BufEnter", {
+                callback = function(ev)
+                    local tab = tostring(vim.api.nvim_get_current_tabpage())
+                    local tags = vim.b[ev.buf].workspace_tabs or {}
+                    tags[tab] = true
+                    vim.b[ev.buf].workspace_tabs = tags
+                end,
+            })
+
+            -- Auto-save all workspaces on exit
+            vim.api.nvim_create_autocmd("VimLeavePre", {
+                callback = function()
+                    resession.save("last", { notify = false })
+                end,
+            })
+        end,
     },
 }
